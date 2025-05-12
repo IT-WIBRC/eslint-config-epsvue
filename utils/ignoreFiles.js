41c105110555bd2d847f5cec00d233b3ecb7c922
IT-WIBRC <wibrc.se.jc@gmail.com>
2025-05-12 17:34:23 +0100

    Fix: Linter eslint does not work as expected whe using typescript

---
+++ utils/ignoreFiles.js
@@ -0,0 +1,14 @@
+export default  [
+    "node_modules/**/*",
+    "**/package-lock.json",
+    ".husky/**/*",
+    ".idea/**/*",
+    "**/.vscode",
+    "build/**/*",
+    "deployment/**/*",
+    "!**/.prettierrc.js",
+    "coverage/",
+    "*.d.ts", 
+    "**/coverage", 
+    "**/dist"
+  ]
